9e8c296103ce8a076ec57adb684617e8270c58a0
zyar3k <a.zygan@gmail.com>
2021-10-26 20:05:09 +0200

    Add routing to project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,10 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
 import Product from "./pages/Product";
@@ -6,7 +13,31 @@ import Login from "./pages/Login";
 import Cart from "./pages/Cart";
 
 const App = () => {
-  return <Home />;
+  const user = true;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/products/:category">
+          <ProductList />
+        </Route>
+        <Route path="/product/:id">
+          <Product />
+        </Route>
+        <Route path="/register">
+          <Route path="/login">
+            {user ? <Redirect to="/" /> : <Register />}
+          </Route>
+        </Route>
+        <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
