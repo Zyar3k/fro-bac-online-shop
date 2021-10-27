4aed714851911bdbcc0755f57cf4b1f9c970a212
zyar3k <a.zygan@gmail.com>
2021-10-27 21:16:28 +0200

    Add info widgets

--- admin/src/App.js
+++ admin/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import Sidebar from "./components/Sidebar/Sidebar";
 import TopBar from "./components/TopBar/TopBar";
+import Home from "./pages/Home/Home";
 
 import "./App.css";
 
@@ -11,6 +12,7 @@ function App() {
       <TopBar />
       <div className="container">
         <Sidebar />
+        <Home />
       </div>
     </Router>
   );
