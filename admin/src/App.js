59d2e40f6cdab1647641eb184bfa1b848abaf9c9
zyar3k <a.zygan@gmail.com>
2021-10-27 18:43:29 +0200

    Add sidebar

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,10 +1,18 @@
+import { BrowserRouter as Router } from "react-router-dom";
+
+import Sidebar from "./components/Sidebar/Sidebar";
 import TopBar from "./components/TopBar/TopBar";
+
+import "./App.css";
 
 function App() {
   return (
-    <div>
+    <Router>
       <TopBar />
-    </div>
+      <div className="container">
+        <Sidebar />
+      </div>
+    </Router>
   );
 }
 
