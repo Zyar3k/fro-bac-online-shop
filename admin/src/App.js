ed8232e5face854112a2425d4ef19aa037e1910d
zyar3k <a.zygan@gmail.com>
2021-10-28 09:49:25 +0200

    Add users list in data grid table

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,10 +1,16 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 import Sidebar from "./components/Sidebar/Sidebar";
 import TopBar from "./components/TopBar/TopBar";
 import Home from "./pages/Home/Home";
 
 import "./App.css";
+import UserList from "./pages/UserList/UserList";
 
 function App() {
   return (
@@ -12,7 +18,14 @@ function App() {
       <TopBar />
       <div className="container">
         <Sidebar />
-        <Home />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/users">
+            <UserList />
+          </Route>
+        </Switch>
       </div>
     </Router>
   );
