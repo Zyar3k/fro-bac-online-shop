9e7e438ad8935c2dd52aece4cdbc0304252bce77
zyar3k <a.zygan@gmail.com>
2021-11-03 14:48:35 +0100

    Fetch users and orders from api

--- admin/src/App.js
+++ admin/src/App.js
@@ -14,6 +14,7 @@ import NewProduct from "./pages/NewProduct/NewProduct";
 import Login from "./pages/Login/Login";
 
 function App() {
+  // const admin = false;
   const admin = JSON.parse(
     JSON.parse(localStorage.getItem("persist:root")).user
   ).currentUser.isAdmin;
