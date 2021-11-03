72af73e6c3399bb565176c7a0c44135fa3aff7f8
zyar3k <a.zygan@gmail.com>
2021-11-03 15:00:46 +0100

    Add calc revenue

--- admin/src/components/FeaturedInfo/FeaturedInfo.jsx
+++ admin/src/components/FeaturedInfo/FeaturedInfo.jsx
@@ -1,16 +1,37 @@
+import { useEffect, useState } from "react";
+import { userRequest } from "../../requestMethods";
+
 import "./FeaturedInfo.css";
 
 import { ArrowDownward, ArrowUpward } from "@material-ui/icons";
 
 const FeaturedInfo = () => {
+  const [income, setIncome] = useState([]);
+  const [perc, setPerc] = useState(0);
+
+  useEffect(() => {
+    const getIncome = async () => {
+      try {
+        const res = await userRequest.get("orders/income");
+        setIncome(res.data);
+        setPerc((res.data[1].total * 100) / res.data[0].total - 100);
+      } catch {}
+    };
+    getIncome();
+  }, []);
   return (
     <div className="featured">
       <div className="featuredItem">
         <span className="featuredTitle">Revenue</span>
         <div className="featuredMoneyContainer">
-          <span className="featuredMoney">$2,415</span>
+          <span className="featuredMoney">${income[1]?.total}</span>
           <span className="featuredMoneyRate">
-            <ArrowDownward className="featuredIcon negative" />
+            {Math.floor(perc)}%
+            {perc < 0 ? (
+              <ArrowDownward className="featuredIcon negative" />
+            ) : (
+              <ArrowUpward className="featuredIcon" />
+            )}
           </span>
         </div>
         <span className="featuredSub">Compared to last month</span>
